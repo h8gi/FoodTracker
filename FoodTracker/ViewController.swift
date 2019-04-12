4f6281a135f76d5d176bcfc4b2b473cfd1ca73be
hiroki yagi <hiro.yagi0245@gmail.com>
2019-04-12 12:01:06 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 八木浩樹 on 2019/04/12.
+//  Copyright © 2019 yagihiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
